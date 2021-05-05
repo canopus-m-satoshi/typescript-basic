f23f25ca496ee7ffeaf06950c8c53ccfc1aff9a2
Satoshi Matushima <58695418+satoshi-matsushima@users.noreply.github.com>
2021-05-05 15:33:41 +0900

    48. 設定ファイル(tsconfig)をいじってみる https://www.udemy.com/course/react_stepup/learn/lecture/25224362#content

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { Practice1 } from "./practices/Practices1";
 import { Practice2 } from "./practices/Practices2";
 import { Practice3 } from "./practices/Practices3";
+import { Practice4 } from "./practices/Practices4";
 import "./styles.css";
 
 export default function App() {
@@ -9,6 +10,7 @@ export default function App() {
       <Practice1 />
       <Practice2 />
       <Practice3 />
+      <Practice4 />
     </div>
   );
 }
