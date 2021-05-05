b5760d71b8bc5746701894d509c5d7cac8d8f159
Satoshi Matushima <58695418+satoshi-matsushima@users.noreply.github.com>
2021-05-05 15:25:44 +0900

    46. 返却値の型指定

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import { Practice1 } from "./practices/Practices1";
+import { Practice2 } from "./practices/Practices2";
 import "./styles.css";
 
 export default function App() {
   return (
     <div className="App">
       <Practice1 />
+      <Practice2 />
     </div>
   );
 }
